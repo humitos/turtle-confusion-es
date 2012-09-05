f2afa57a5fc7c7c4d8c61ea9fbc7b77590c510e8
Manuel Kaufmann <humitos@gmail.com>
2012-09-05 13:17:05 -0300

    User rst2pdf inkscape extension
    This is because I was having an issue with the Puzzle 4. The little
    triangle was not drawn as I expected.

--- make_pdf.sh
+++ make_pdf.sh
@@ -1,1 +1,1 @@
-rst2pdf docs/index.rst --output=turtle-confusion-es.pdf --default-dpi=62 --stylesheets=docs/_styles/my.style --break-level=2 --language=es_AR --smart-quotes=1
+rst2pdf docs/index.rst --extension=inkscape --output=turtle-confusion-es.pdf --default-dpi=62 --stylesheets=docs/_styles/my.style --break-level=2 --language=es_AR --smart-quotes=1
